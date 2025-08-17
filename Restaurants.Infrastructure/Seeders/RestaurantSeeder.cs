63938e8e5d34d48c6908d19268dcfcd6d1c7e98c
Saulo Campos <97260136+saulo-de-tarso@users.noreply.github.com>
2025-08-17 02:23:37 -0300

    Adding layers and populating domain and infrastructure

---
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -0,0 +1,71 @@
+﻿using Restaurants.Domain.Entities;
+using Restaurants.Infrastructure.Persistence;
+
+namespace Restaurants.Infrastructure.Seeders;
+
+internal class RestaurantSeeder(RestaurantsDbContext dbContext) : IRestaurantSeeder
+{
+    public async Task Seed()
+    {
+        if (await dbContext.Database.CanConnectAsync())
+        {
+            if (!dbContext.Restaurants.Any())
+            {
+                var restaurants = GetRestaurants();
+                dbContext.Restaurants.AddRange(restaurants);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+
+    private IEnumerable<Restaurant> GetRestaurants()
+    {
+        List<Restaurant> restaurants = [
+            new()
+            {
+                Name = "KFC",
+                Category = "Fast Food",
+                Description = "KFC is an American fast food restaurant.",
+                ContactEmail = "contact@kfc.com",
+                HasDelivery = true,
+                Dishes =
+                [
+                    new ()
+                    {
+                        Name = "Nashville Hot Chicken",
+                        Description = "Nashville Hot Chicken (10 pcs.)",
+                        Price = 10.30M
+                    },
+                    new ()
+                    {
+                        Name = "Chicken Nuggets",
+                        Description = "Chicken Nuggets (5 pcs.)",
+                        Price = 5.30M
+                    },
+                ],
+                Address = new ()
+                {
+                    City = "London",
+                    Street = "Cork St 5",
+                    PostalCode = "WC2N 5DU"
+                }
+            },
+            new()
+            {
+                Name = "McDonalds",
+                Category = "Fast Food",
+                Description = "Delicious fast food",
+                ContactEmail = "contact@mcdonalds.com",
+                HasDelivery = true,
+                Address = new ()
+                {
+                    City = "London",
+                    Street = "Boots 193",
+                    PostalCode = "W1F 8SR"
+                }
+            }
+        ];
+
+        return restaurants;
+    }
+}
