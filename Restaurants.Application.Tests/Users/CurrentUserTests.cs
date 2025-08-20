4a8cb0cea7f5f0631013deefeae86bc83c6fd845
Saulo Campos <97260136+saulo-de-tarso@users.noreply.github.com>
2025-08-19 22:19:26 -0300

    Creating user tests

--- Restaurants.Application.Tests/Users/CurrentUserTests.cs
+++ Restaurants.Application.Tests/Users/CurrentUserTests.cs
@@ -1,12 +1,61 @@
-﻿using Xunit;
+﻿using FluentAssertions;
+using Restaurants.Domain.Constants;
+using Xunit;
 
 namespace Restaurants.Application.Users.Tests;
 
 public class CurrentUserTests
 {
-    [Fact()]
-    public void IsInRoleTest()
+    // TestMethod_Scenario_ExpectedResult
+    [Theory()]
+    [InlineData(UserRoles.Admin)]
+    [InlineData(UserRoles.User)]
+    public void IsInRole_WithMatchingRole_ShouldReturnTrue(string roleName)
     {
+        //arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User]);
+
+        //act
+
+        var isInRole = currentUser.IsInRole(roleName);
+
+        //assert
+
+        isInRole.Should().BeTrue();
+
+    }
+
+    // TestMethod_Scenario_ExpectedResult
+    [Fact()]
+    public void IsInRole_WithNoMatchingRole_ShouldReturnTrue()
+    {
+        //arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User]);
+
+        //act
+
+        var isInRole = currentUser.IsInRole(UserRoles.Owner);
+
+        //assert
+
+        isInRole.Should().BeFalse();
+
+    }
+
+    // TestMethod_Scenario_ExpectedResult
+    [Fact()]
+    public void IsInRole_WithNoMatchingRoleCase_ShouldReturnTrue()
+    {
+        //arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User]);
+
+        //act
+
+        var isInRole = currentUser.IsInRole(UserRoles.Admin.ToLower());
+
+        //assert
+
+        isInRole.Should().BeFalse();
 
     }
 }
---
+++ Restaurants.Application.Tests/Users/UserContextTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Restaurants.Domain.Constants;
+using System.Security.Claims;
+using Xunit;
+
+namespace Restaurants.Application.Users.Tests;
+
+public class UserContextTests
+{
+    [Fact()]
+    public void GetCurrentUser_WithAutenticatedUser_ShouldReturnCurrentUser()
+    {
+        //arrange
+        var httpContextAcessorMock = new Mock<IHttpContextAccessor>();
+        var claims = new List<Claim>()
+        {
+            new(ClaimTypes.NameIdentifier, "1"),
+            new(ClaimTypes.Email, "test@test.com"),
+            new(ClaimTypes.Role, UserRoles.Admin),
+            new(ClaimTypes.Role, UserRoles.User)
+        };
+
+        var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+
+        httpContextAcessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+        {
+            User = user
+
+        });
+
+        var userContext = new UserContext(httpContextAcessorMock.Object);
+
+        //act
+
+        var currentUser = userContext.GetCurrentUser();
+
+        //assert
+
+        currentUser.Should().NotBeNull();
+        currentUser.Id.Should().Be("1");
+        currentUser.Email.Should().Be("test@test.com");
+        currentUser.Roles.Should().ContainInOrder(UserRoles.Admin, UserRoles.User);
+
+    }
+
+    [Fact]
+    public void GetCurrentUser_WithUserContextNotPresent_ThrowsInvalidOperationException()
+    {
+        //Arrange
+
+        var httpContextAcessorMock = new Mock<IHttpContextAccessor>();
+        httpContextAcessorMock.Setup(x => x.HttpContext).Returns((HttpContext)null);
+
+        var userContext = new UserContext(httpContextAcessorMock.Object);
+
+        //act
+
+        Action action = () => userContext.GetCurrentUser();
+
+        //assert
+
+        action.Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("User context is not present.");
+
+    }
+}
